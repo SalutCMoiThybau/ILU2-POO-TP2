0fe6b3fc5bcd9025796a448f922fbacd0890c24b
Thybau Perez--resseguier <prt4545a@ens.fsi>
2023-04-05 17:40:18 +0200

    prendreEtal à finir

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,23 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		Boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (!nomVendeurConnu) System.out.println("Je suis désolé " + nomVendeur + " mais il faut être un habitant de notre village pour commencer ici.\n");
+		else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.\n");
+			
+			Boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (!etalDisponible)System.out.println("Désolé " + nomVendeur + ", je n'ai plus d'étal qui ne soit pas déjà occupé.\n");
+			else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		StringBuilder chaine =new StringBuilder();
+		chaine.append("C'est parfait, il me reste un étal pour vous !\n");
+		chaine.append("Il me faudrait quelques renseignements :\n");
+		//TODO
 	}
 }
